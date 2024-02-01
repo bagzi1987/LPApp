4e4cde4099ddf254526164c28645eaf80785755c
bagzi1987 <59961500+bagzi1987@users.noreply.github.com>
2024-02-01 17:49:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ let showPassword = () => {
         inputPassword.type = "password";
     }
 }
+
+
+
+// VALIDATE FORM AND LOG IN
+function validateForm() {
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+    let submitBtn = document.getElementById("submitBtn");
+
+    if (username.value == "" || password.value == "") {
+        alert("Unesite i korisnicko ime i lozinku");
+        username.value = "";
+        password.value = "";
+    }
+
+    if(username.value === "stojan" && password.value === "stojan123") {
+        console.log(`Welcome, ${username}`);
+        window.location.href = "home.html";
+    } else {
+        console.log("Wrong username or password");
+    }
+}
+
